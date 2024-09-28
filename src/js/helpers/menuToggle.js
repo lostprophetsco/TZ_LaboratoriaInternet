22ed82d54de486e925dc08c9fce28d47353586de
Vadim <lostprophetsco@gmail.com>
2024-09-28 17:55:56 +0300

    fix: refactor header css & improve menuToggle js

--- src/js/helpers/menuToggle.js
+++ src/js/helpers/menuToggle.js
@@ -1,5 +1,10 @@
+import throttle from './throttle.js';
 /**
- * @description Menu && classes toggle
+ * Adds click event listener to the burger button and toggles classes,
+ * ARIA attribute and body overflow property.
+ *
+ * @function
+ * @returns {undefined}
  */
 const menuToggle = () => {
   const logo = document.querySelector('.header__logo');
@@ -11,7 +16,18 @@ const menuToggle = () => {
     burger.classList.toggle('burger--active');
     burger.attributes['aria-expanded'].value = burger.classList.contains('burger--active');
     nav.classList.toggle('header__nav--active');
+    document.body.classList.toggle('overflow-hidden');
   });
+
+  const removeClassesPerWidth = throttle(() => {
+    logo.classList.remove('header__logo--active');
+    burger.classList.remove('burger--active');
+    burger.classList.remove('burger--active');
+    nav.classList.remove('header__nav--active');
+    document.body.classList.remove('overflow-hidden');
+  }, 500);
+
+  window.addEventListener('resize', removeClassesPerWidth);
 };
 
 export { menuToggle };
